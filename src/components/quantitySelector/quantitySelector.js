e00d0f4b693ec5b4b38cf215e845c95c12b7eb6d
rheimers <68224228+rheimers@users.noreply.github.com>
2020-07-29 14:08:43 +0200

    Add quantitySelector-js (#12)
    * Delete details-js due to  conflicts
    
    * Add minus svg to assets, Modify in html
    
    * Add quantitySelector-js Modify images HTML
    
    * Add quantitySelector Modify images HTMl
    
    * Add element-js

---
+++ src/components/quantitySelector/quantitySelector.js
@@ -0,0 +1,55 @@
+import minusSrc from "../../assets/minus.svg";
+import plusSrc from "../../assets/plus.svg";
+import { createElement } from "../../utils/element";
+
+export const createQuantitySelector = () => {
+  //Outer div
+  const quantitySelector = document.createElement("div");
+  quantitySelector.className = "quantitySelector";
+
+  //First button (Minus)
+  const minusButton = document.createElement("button");
+  minusButton.className = "quantitySelector_minus";
+  minusButton.innerHTML = "-";
+
+  quantitySelector.append(minusButton);
+  return quantitySelector;
+};
+
+//Outer div
+const quantitySelector = createElement("div", {
+  className: "quantitySelector",
+});
+
+//First button (Minus)
+const minusButton = createElement("button", {
+  className: "quantitySelector__minus",
+  disabled: true,
+});
+
+//Minus image
+const minusImage = document.createElement("img", {
+  src: minusSrc,
+  alt: "Minus",
+});
+
+minusButton.append(minusImage);
+
+// Result
+const resultElement = createElement("div", {
+  className: "quantitySelector__result",
+  innerText: "1",
+});
+
+//Second button (Plus)
+const plusButton = createElement("button", {
+  className: "quantitySelector__plus",
+});
+
+//Plus image
+const plusImage = createElement("img", {
+  src: plusSrc,
+  alt: "Plus",
+});
+
+plusButton.append(plusImage);
